e5289b736465bdd2539952c1a3b2e07d64a315dc
Alexandra Maria Bosca <ambosca2@iesfuentesanluis.org>
2020-04-27 20:59:06 +0200

    Cambio Alexandra y Donet

--- src/maquina2_2020/DispensadorDeBotes.java
+++ src/maquina2_2020/DispensadorDeBotes.java
@@ -7,8 +7,36 @@ package maquina2_2020;
 
 /**
  *
- * @author Rosana
+ * @author Alexandra Mº Bosca y Alejandro Donet
  */
 public class DispensadorDeBotes {
     
+    private int botes; 
+    boolean existenBotes = false;
+
+    public DispensadorDeBotes(int bote){
+        iniciarDispensadorDeBotes(bote);
+    }    
+ 
+    
+    public void iniciarDispensadorDeBotes (int i){
+        botes = i;
+    }
+    
+    public boolean pulsarBoton (){
+        
+        if(botes>0){
+            existenBotes= true;
+        botes--;
+        }
+        
+        else
+            existenBotes= false;
+        return existenBotes;
+    }
+    
+    
+    public int botesDisponibles (){
+        return botes;
+    }
 }
